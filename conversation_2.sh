08bfbce1b29f8ad022fcc194045f08f73bba09a9
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-26 18:48:35 +0530

    variables added

--- conversation_2.sh
+++ conversation_2.sh
@@ -3,9 +3,9 @@
 name_1=Hemanth
 name_2=Supreme
 
-echo "$name_1: Hi $name_2, How are you?"
-echo "$name_2: Hello $name_1, Thanks I'm doing great. what about you?"
-echo "$name_1: I'm also doing great $name_2."
-echo "$name_2: Then how is your new learning?"
-echo "$name_1: yeah! it is going excellent."
-echo "$name_2: catch you there."
+echo "{$name_1}: Hi {$name_2}, How are you?"
+echo "{$name_2}: Hello {$name_1}, Thanks I'm doing great. what about you?"
+echo "{$name_1}: I'm also doing great {$name_2}."
+echo "{$name_2}: Then how is your new learning?"
+echo "{$name_1}: yeah! it is going excellent."
+echo "{$name_2}: catch you there."
