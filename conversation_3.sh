237a6fe016fbde50906d33a2033800d2a4b8c960
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-26 19:01:22 +0530

    with arguments

---
+++ conversation_3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "$name_1: Hi $name_2, How are you?"
+echo "$name_2: Hello $name_1, Thanks I'm doing great. what about you?"
+echo "$name_1: I'm also doing great $name_2."
+echo "$name_2: Then how is your new learning? $name_1"
+echo "$name_1: yeah! it is going excellent $name_2."
+echo "$name_2: catch you there $name_1."
