f0b32465e14e6c34ad0b1a69934defa517863940
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-26 18:01:06 +0530

    conversation

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hemanth: Hi Supreme, How are you?"
+echo "Supreme: Hello Hemanth, Thanks I'm doing great. what about you?"
+echo "Hemanth: I'm also doing great."
+echo "Supreme: Then how is your new learning?"
+echo "Hemanth: yeah! it is going excellent."
