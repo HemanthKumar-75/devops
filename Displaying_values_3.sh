b4d31c78db0f022426fc60a55be23fb394e82477
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-26 19:23:27 +0530

    display/hide values

---
+++ Displaying_values_3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -n "Please enter your name : "
+read NAME
+echo "Entered name is : $NAME"
+
+echo -n "Please enter your password : "
+read -s PASS
+echo "Entered password is : $PASS"
