f266e1e106c5493dbb532ebd6e546d9f2b98ac49
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-26 19:05:28 +0530

    with arguments

--- conversation_3.sh
+++ conversation_3.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+name_1=$1
+name_2=$2
+
 echo "$name_1: Hi $name_2, How are you?"
 echo "$name_2: Hello $name_1, Thanks I'm doing great. what about you?"
 echo "$name_1: I'm also doing great $name_2."
