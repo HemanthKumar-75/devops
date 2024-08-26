af1003ad7952be8519451dfb0acdcbdebdc3721f
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-26 18:21:52 +0530

    conversation

--- conversation_2.sh
+++ conversation_2.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-name_1=$Hemanth
-name_2=$Supreme
+name_1=$"Hemanth"
+name_2=$"Supreme"
 echo "$name_1: Hi $name_2, How are you?"
 echo "$name_2: Hello $name_1, Thanks I'm doing great. what about you?"
 echo "$name_1: I'm also doing great $name_2."
