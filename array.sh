af36a565b52b6a0ddf42a436be89081edd357a12
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-26 19:38:44 +0530

    display values

--- array.sh
+++ array.sh
@@ -2,9 +2,9 @@
 
 FRUITS=("Apple" "Banana" "Kiwi" "Orange")
 
-echo "First Fruit is : $FRUITS[0]"
-echo "Second Fruit is : $FRUITS[1]"
-echo "Third Fruit is : $FRUITS[2]"
-echo "Fourth Fruit is : $FRUITS[3]"
+echo "First Fruit is : ${FRUITS[0]}"
+echo "Second Fruit is : ${FRUITS[1]}"
+echo "Third Fruit is : ${FRUITS[2]}"
+echo "Fourth Fruit is : ${FRUITS[3]}"
 
-$FRUITS[@]
+${FRUITS[@]}
