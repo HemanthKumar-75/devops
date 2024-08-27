d62abde867319f179c7cc519a94a1a5944c8214a
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-27 19:55:01 +0530

    installations

--- installations.sh
+++ installations.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 id=$(id -u)
+#echo "id details are : $id"
 
-echo "id details are : $id"
+if [ $id -eq 0 ]
+then
+    dnf list installed mysql
+else
+    echo "please try to run with root previlages"
+    exit 1
