1ee99a6dc251c9f735fca7157e47fe341d787b18
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-27 19:18:41 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -2,10 +2,10 @@
 echo -n "enter your number here : "
 read num
 
-if [num -gt 20]
+if [ $num -gt 20 ]
 then
     echo "Given number $num is greater than 20"
-elif [num -lt 20]
+elif [ $num -lt 20 ]
     echo "Given number $num is lesser than 20"
 else
     echo "Givne number $num is equla to 20"
