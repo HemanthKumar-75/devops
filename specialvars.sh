51075d21eefbd7280649192f11cfc5bf2f72d3fd
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-27 18:57:41 +0530

    special vars

---
+++ specialvars.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "All arguments : $@"
+
+echo "Number of arguments : $#"
+
+echo "Script name : $0"
+
+echo "Present working directory where script executed from : $PWD"
+
+echo "Home directory : $HOME"
+
+echo "process ID of current script : $$"
+
+echo "Process ID of last background command : $!"
