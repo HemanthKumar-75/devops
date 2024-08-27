a6596ce541a7c03196445b894ca50ed727dbad0d
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-27 19:23:59 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 echo -n "enter your number here : "
 read num
 
 if [ $num -gt 20 ]
 then
-    echo "Given number $num is greater than 20"
+    echo -e "$G Given number $num is greater than 20 $N"
 elif [ $num -lt 20 ]
 then
     echo "Given number $num is lesser than 20"
