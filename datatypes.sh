1d024124dfb0dd7017d886258a9d59963b69485b
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-27 14:31:53 +0530

    time

--- datatypes.sh
+++ datatypes.sh
@@ -8,7 +8,7 @@ MUL=$(($number1*$number2))
 DIV=$(($number1/$number2))
 SUB=$(($number1-$number2))
 
-echo "summation of $number1 and $number2 is equals to : $SUM"
+echo "summation of $number1 and $number2 is equals to : $sum"
 echo "multiplication of $number1 and $number2 is equals to : $MUL"
 echo "division of $number1 and $number2 is equals to : $DIV"
 echo "substraction of $number1 and $number2 is equals to : $SUB"
