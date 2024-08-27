4dbbdf3aed2d9b5f52275ee9d64e82a465b73546
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-27 20:27:06 +0530

    installations

--- installations.sh
+++ installations.sh
@@ -3,10 +3,25 @@
 id=$(id -u)
 #echo "id details are : $id"
 
-if [ $id -eq 0 ]
+if [ $id -ne 0 ]
 then
-    dnf list installed mysql
-else
     echo "please try to run with root previlages"
     exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MYSQL is not installed, going to install..."
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "MYSQL installation got failure... check it..."
+        exit 1
+    else
+        echo "MYSQL installation is SUCCESSFUL."
+    fi
+else
+    echo "MYSQL is already installed."
 fi
