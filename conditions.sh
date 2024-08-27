12c3198766bd4997d02f621e76c846386adf5d87
HemanthKumar-75 <hemanth4125@gmail.com>
2024-08-27 19:26:13 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
 echo -n "enter your number here : "
@@ -11,7 +12,7 @@ then
     echo -e "$G Given number $num is greater than 20 $N"
 elif [ $num -lt 20 ]
 then
-    echo "Given number $num is lesser than 20"
+    echo -e "$R Given number $num is lesser than 20 $N"
 else
-    echo "Givne number $num is equla to 20"
+    echo -e "$Y Givne number $num is equla to 20 $N"
 fi
